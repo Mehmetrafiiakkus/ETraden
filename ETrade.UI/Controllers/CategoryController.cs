00d7b62418b6e46ea372cd52199033c2ba61c665
Devrim Mehmet Pattabanoğlu <mehmet.akkus@302-18>
2023-01-06 10:37:36 +0300

    Add project files.

---
+++ ETrade.UI/Controllers/CategoryController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ETrade.Data.Models.Entites;
+using ETrade.Dal.Abstract;
+using ETrade.Dal.Concrete;
+
+namespace ETrade.UI.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ICategoryDAl categoryDAL;
+        //CategoryDAL _categoryDAL = new CategoryDAL();
+        public CategoryController(ICategoryDAl categoryDAL)
+        {
+          this.categoryDAL = categoryDAL;   
+        }
+        public IActionResult Index()
+        {
+            return View(categoryDAL.GetAll());
+        }
+        public IActionResult Create() =>View();
+        public IActionResult Create(Category category)
+        {
+            if (ModelState.IsValid)
+            {
+                categoryDAL.Add(category);
+                return RedirectToAction("Index"); 
+            }
+            return View();
+        }
+
+
+    }
+}
